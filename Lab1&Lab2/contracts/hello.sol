dfedd75c06f1c057e510e6e5d79c8569942e01d0
Bruno <brunomazorra@gmail.com>
2023-04-24 10:20:01 +0200

    change folder name

--- Lab1&Lab2/contracts/hello.sol
+++ Lab1&Lab2/contracts/hello.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0; // Version of Solidity
 contract HelloWorld {
     event Message(string message);
 
-    function sendHelloWorld() external {
+    function sendHelloWorld() external payable {
         require(msg.value == 10 ether, "You must send exactly 10 ETH.");
         emit Message("Hello World");
     }
